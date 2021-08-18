b8dce5b0a2810bdc8d7aa42d294f19df6cc7f9a1
Rodrigo Aguilar <rodrigoaguilar96@gmail.com>
2021-08-18 19:41:21 -0300

    clase 5

--- practica/clase 4/js/main.js
+++ practica/clase 4/js/main.js
@@ -1,3 +1,6 @@
+
+const url = 'https://610f5d459b698d0017175310.mockapi.io/api/v1/Personas';
+
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("#crearPersona").addEventListener("click", () => {
         let nombre = document.querySelector("#nombre").value;
@@ -8,12 +11,14 @@ document.addEventListener("DOMContentLoaded", () => {
         let edad = document.querySelector("#edad").value;
         console.log(document.querySelector("#apellido"));
         crearPersona(nombre,apellido,direccion,titulo,avatar,edad);
-    })
+    });
+    getPersonas();
+
 })
 
 
 async function crearPersona(nombre, apellido, direccion, titulo, avatar, edad) {
-    await fetch("https://610f5d459b698d0017175310.mockapi.io/api/v1/Personas", {
+    await fetch(url, {
         method: "POST",
         headers: {
             'Accept': 'application/json',
@@ -36,6 +41,60 @@ async function crearPersona(nombre, apellido, direccion, titulo, avatar, edad) {
             throw new Error("no pude insertar")
         }
     }).catch((error) =>  {
-        console.log("falle porque " + error);
+        console.error("falle porque " + error);
     })
 }
+
+async function getPersonas() {
+    await fetch(url)
+        .then(response => {
+        response.json().then(resp => {
+            let tablePersona = document.querySelector("#personas");
+            tablePersona.innerHTML = "";
+
+            //Esto retorna el nombre de los campos
+            //Para realizar esto añadir validaciones
+            console.log(Object.keys(resp[0]));
+
+            resp.map(persona => {
+                console.log(persona);
+                let tablerow = document.createElement("tr");
+                let nombreColumna = document.createElement("td");
+                let nombre = document.createTextNode(persona.nombre);
+                nombreColumna.append(nombre);
+                tablerow.append(nombreColumna);
+
+                let apellido = document.createElement("td");
+                apellido.append(document.createTextNode(persona.apellido))
+                tablerow.append(apellido)
+
+                let direccion = document.createElement("td")
+                direccion.append(document.createTextNode(persona.direccion));
+                tablerow.append(direccion)
+
+                let eliminar = document.createElement("button");
+                eliminar.setAttribute("value",persona.id);
+                eliminar.append(document.createTextNode("Eliminar"));
+                tablerow.append(eliminar);
+                eliminar.addEventListener("click", ()=> {
+                    eliminarPersona(eliminar.getAttribute("value"));
+                })
+
+                tablePersona.append(tablerow);
+            })
+        })
+    })
+}
+
+
+async function eliminarPersona(id) {
+    await fetch(url + "/" + id, {
+        method: "DELETE"
+    }).then( (response)=> {
+        if (response.ok) {
+            getPersonas();
+        }
+    })
+
+}
+
