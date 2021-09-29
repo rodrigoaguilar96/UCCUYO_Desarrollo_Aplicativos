65733e01ebcb85e252d3b584335f7a770a9162cb
Rodrigo Aguilar <rodrigoaguilar96@gmail.com>
2021-09-29 17:57:31 -0300

    HeroesAppTest

---
+++ practica/heroesAppTest/src/app/auth/auth-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {LoginComponent} from "./pages/login/login.component";
+import {RegistroComponent} from "./pages/registro/registro.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'registro',
+        component: RegistroComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'login'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AuthRoutingModule { }
